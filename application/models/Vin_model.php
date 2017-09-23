3e820969c512a1e8d52589dbbfe9d237e57b4ee3
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-23 10:50:08 +0800

    Refactor vin model

--- application/models/Vin_model.php
+++ application/models/Vin_model.php
@@ -13,82 +13,17 @@ class Vin_model extends CI_Model {
 	{
 		if ($params['type'] == 'object')
 		{
-			return $this->db->get('category_tbl')->result();	
+			return $this->db->get('vin_model_tbl')->result();	
 		}
 
-		return $this->db->get('category_tbl')->result_array();
-	}
-
-	public function fetch_category_items()
-	{
-		$categories = $this->browse();
-
-		$config = array();
-
-		$fields = array(
-				'a.category_id',
-				'b.id',
-				'b.name',
-				'b.price',
-				'b.thumbnail',
-				'b.barcode'
-			);
-
-		foreach($categories as $category)
-		{
-			$query = $this->db->select($fields)
-					->from('item_category_tbl AS a')
-					->join('items_tbl AS b', 'a.item_id = b.id', 'INNER')
-					->where('a.category_id', $category->id)
-					->get();
-
-			if ($query->num_rows())
-			{
-				array_push($config, $query->result_array());
-			}
-		}
-
-		return $config;
-	}
-
-	public function fetch_featured_items()
-	{
-		$categories = $this->browse();
-
-		$config = array();
-
-		$fields = array(
-				'b.id',
-				'b.name',
-				'b.price',
-				'b.thumbnail',
-				'b.barcode',
-				'c.category_id'
-			);
-
-		foreach ($categories as $category)
-		{
-			$query = $this->db->select($fields)
-				->from('featured_items_tbl AS a')
-				->join('items_tbl AS b', 'a.item_id = b.id', 'INNER')
-				->join('item_category_tbl AS c', 'b.id = c.item_id', 'INNER')
-				->where('c.category_id', $category->id)
-				->get();
-
-			if ($query->num_rows())
-			{
-				array_push($config, $query->result_array());
-			}
-		}
-
-		return $config;
+		return $this->db->get('vin_model_tbl')->result_array();
 	}
 
 	public function read(array $params = array('type' => 'object'))
 	{
 		if ($params['id'] > 0)
 		{
-			$query = $this->db->get_where('category_tbl', array('id' => $params['id']));
+			$query = $this->db->get_where('vin_model_tbl', array('id' => $params['id']));
 
 			if ($params['type'] == 'object')
 			{
@@ -99,35 +34,35 @@ class Vin_model extends CI_Model {
 		}
 	}
 
-	public function store()
+	public function store($params)
 	{
-		$id       = $this->input->post('id') ? $this->input->post('id') : 0;
-		$name     = ucfirst(strtolower(trim($this->input->post('name'))));
-		$datetime = date('Y-m-d H:i:s');
-
-		$config = array(
-				'name'     => $name,
-				'datetime' => $datetime
-			);
+		$id = $this->input->post('id') ? $this->input->post('id') : 0;
 
 		if ($id > 0)
 		{
-			$this->db->update('category_tbl', $config, array('id' => $id));
+			$this->db->update('vin_model_tbl', $params, array('id' => $id));
 		}
 		else
 		{
-			$this->db->insert('category_tbl', $config);
+			$this->db->insert('vin_model_tbl', $params);
 		}
 
-		return $this;
+		return $this;	
 	}
 
 	public function delete()
 	{
 		$id = $this->input->post('id');
 
-		$this->db->delete('category_tbl', array('id' => $id));
+		$this->db->delete('vin_model_tbl', array('id' => $id));
 
 		return $this;
+	}
+
+	public function exist($params)
+	{
+		$query = $this->db->get_where('vin_model_tbl', array('product_model' => $params['product_model']));
+
+		return $query->num_rows();
 	}
 }
