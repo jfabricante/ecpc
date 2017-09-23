6600b4802d6a346ee2d9e487119f77ee0811ba8f
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-23 10:53:56 +0800

    Vin control controller

---
+++ application/controllers/Vin_control.php
@@ -0,0 +1,79 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Vin_control extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('vin_control_model', 'vin_control');
+	}
+
+	public function list_()
+	{
+		$data = array(
+				'title'    => 'List of Vin Control',
+				'content'  => 'vin_control/list_view',
+				'entities' => $this->vin_control->browse()
+			);
+
+		$this->load->view('include/template', $data);
+	}
+
+	public function form()
+	{
+		$id = $this->uri->segment(3) ? $this->uri->segment(3) : 0;
+
+		$config = array(
+				'id'   => $id,
+				'type' => 'object'
+			);
+
+		$data = array(
+				'title'   => $id ? 'Update Details' : 'Add Vin Control',
+				'entity'  => $id ? $this->vin_control->read($config) : ''
+			);
+
+		$this->load->view('vin_control/form_view', $data);
+	}
+
+	public function store()
+	{
+		$id = $this->input->post('id') ? $this->input->post('id') : 0;
+
+		$this->vin_control->store();
+
+		if ($id > 0)
+		{
+			$this->session->set_flashdata('message', '<div class="alert alert-success">Vin model has been updated!</div>');
+		}
+		else
+		{
+			$this->session->set_flashdata('message', '<div class="alert alert-success">Vin model has been added!</div>');
+		}
+
+		redirect($this->agent->referrer());
+	}
+
+	public function notice()
+	{
+		$data['id'] = $this->uri->segment(3);
+
+		$this->load->view('vin_control/delete_view', $data);
+	}
+
+	public function delete()
+	{
+		$this->vin_control->delete();
+
+		$this->session->set_flashdata('message', '<div class="alert alert-success">Vin model has been deleted!</div>');
+
+		redirect($this->agent->referrer());
+	}
+
+	public function ajax_category_list()
+	{
+		echo json_encode($this->vin_control->browse());
+	}
+}
