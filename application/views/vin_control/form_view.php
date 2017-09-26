70f64d7972e7a9b4495b02e7bac0b5a61e3c6120
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-26 13:13:54 +0800

    Modify input label and markup

--- application/views/vin_control/form_view.php
+++ application/views/vin_control/form_view.php
@@ -1,4 +1,4 @@
-<form action="<?php echo base_url('index.php/vin/store'); ?>" method="post">
+<form action="<?php echo base_url('index.php/vin_control/store'); ?>" method="post">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 			<span aria-hidden="true">×</span>
@@ -8,27 +8,32 @@
 
 	<div class="modal-body">
 		<div class="form-group hidden">
-			<input type="number" class="form-control" id="id" name="id" value="<?php echo isset($entity->id) ? $entity->id : ''; ?>">
+			<input type="number" class="form-control" id="id" name="id" value="<?php echo isset($entity->id) ? $entity->id : 0; ?>">
 		</div>
 
 		<div class="form-group">
-			<label for="product_model">Product Model</label>
+			<label for="code">Code</label>
+			<input type="text" class="form-control" id="code" name="code" value="<?php echo isset($entity->code) ? $entity->code : ''; ?>">
+		</div>
+
+		<div class="form-group">
+			<label for="vin_no">Vin No.</label>
+			<input type="text" class="form-control" id="vin_no" name="vin_no" value="<?php echo isset($entity->vin_no) ? $entity->vin_no : ''; ?>" required>
+		</div>
+
+		<div class="form-group">
+			<label for="lot_no">Lot No.</label>
+			<input type="text" class="form-control" id="lot_no" name="lot_no" value="<?php echo isset($entity->lot_no) ? $entity->lot_no : ''; ?>" required>
+		</div>
+
+		<div class="form-group">
+			<label for="product_model">Model</label>
 			<input type="text" class="form-control" id="product_model" name="product_model" value="<?php echo isset($entity->product_model) ? $entity->product_model : ''; ?>" required>
 		</div>
 
 		<div class="form-group">
-			<label for="product_name">Product Year</label>
-			<input type="text" class="form-control" id="product_year" name="product_year" value="<?php echo isset($entity->product_year) ? $entity->product_year : ''; ?>" required>
-		</div>
-
-		<div class="form-group">
-			<label for="description">Description</label>
-			<input type="text" class="form-control" id="description" name="description" value="<?php echo isset($entity->description) ? $entity->description : ''; ?>" required>
-		</div>
-
-		<div class="form-group">
-			<label for="lot_size">Lot Size</label>
-			<input type="text" class="form-control" id="lot_size" name="lot_size" value="<?php echo isset($entity->lot_size) ? $entity->lot_size : ''; ?>" required>
+			<label for="model_name">Model Name</label>
+			<input type="text" class="form-control" id="model_name" name="model_name" value="<?php echo isset($entity->model_name) ? $entity->model_name : ''; ?>" required>
 		</div>
 	</div>
 	
