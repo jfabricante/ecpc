db194aeae1a871764ebd8f2e07fa3cb277756ace
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-26 13:14:15 +0800

    Vin control list view

--- application/views/vin_control/list_view.php
+++ application/views/vin_control/list_view.php
@@ -1,17 +1,17 @@
 <!-- Items block -->
-<section class="content vin_model">
+<section class="content vin_control">
 	<!-- row -->
 	<div class="row">
 		<!-- col-md-6 -->
-		<div class="col-md-6">
+		<div class="col-md-9">
 			<!-- Box danger -->
 			<?php echo $this->session->flashdata('message'); ?>
 
 			<div class="box box-danger">
 				<!-- Content -->
 				<div class="box-header with-border">
-					<a href="<?php echo base_url('index.php/vin/form') ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
-						<button class="btn btn-flat btn-success pull-right">Add Vin Model <i class="fa fw fa-plus" aria-hidden="true"></i></button>
+					<a href="<?php echo base_url('index.php/vin_control/form') ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+						<button class="btn btn-flat btn-success pull-right">Add Vin Control <i class="fa fw fa-plus" aria-hidden="true"></i></button>
 					</a>
 				</div>
 
@@ -21,10 +21,13 @@
 						<thead>
 							<tr>
 								<th>#</th>
-								<th>Product Model</th>
-								<th>Product Year</th>
-								<th>Description</th>
-								<th>Lot Size</th>
+								<th>Code</th>
+								<th>Vin No.</th>
+								<th>Lot No.</th>
+								<th>Model</th>
+								<th>Model Name</th>
+								<th>Last User</th>
+								<th>Last Update</th>
 								<th></th>
 								<th></th>
 							</tr>
@@ -35,17 +38,20 @@
 							<?php foreach ($entities as $entity): ?>
 								<tr>
 									<td><?php echo $count; ?></td>
+									<td><?php echo $entity->code; ?></td>
+									<td><?php echo $entity->vin_no; ?></td>
+									<td><?php echo $entity->lot_no; ?></td>
 									<td><?php echo $entity->product_model; ?></td>
-									<td><?php echo $entity->product_year; ?></td>
-									<td><?php echo $entity->description; ?></td>
-									<td><?php echo $entity->lot_size; ?></td>
+									<td><?php echo $entity->model_name; ?></td>
+									<td><?php echo $entity->last_user; ?></td>
+									<td><?php echo $entity->last_update; ?></td>
 									<td>
-										<a href="<?php echo base_url('index.php/vin/form/' . $entity->id); ?>"  data-toggle="modal" data-target=".bs-example-modal-sm">
+										<a href="<?php echo base_url('index.php/vin_control/form/' . $entity->id); ?>"  data-toggle="modal" data-target=".bs-example-modal-sm">
 											<i class="fa fa-pencil" aria-hidden="true"></i>
 										</a>
 									</td>
 									<td>
-										<a href="<?php echo base_url('index.php/vin/notice/' . $entity->id); ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+										<a href="<?php echo base_url('index.php/vin_control/notice/' . $entity->id); ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
 											<i class="fa fa-trash" aria-hidden="true"></i>
 										</a>
 									</td>
