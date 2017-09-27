d17780b0b262f1ce3be0d9138cf01da3f771bd37
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-27 15:59:12 +0800

    Initial template of portcode

---
+++ application/views/portcode/form_view.php
@@ -0,0 +1,64 @@
+<link href="<?php echo base_url('resources/plugins/select/css/bootstrap-select.min.css');?>" rel="stylesheet" >
+<form action="<?php echo base_url('index.php/vin/store'); ?>" method="post">
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">×</span>
+		</button>
+		<h4 class="modal-title"><?php echo $title; ?></h4>
+	</div>
+
+	<div class="modal-body">
+		<div class="form-group hidden">
+			<input type="number" class="form-control" id="id" name="id" value="<?php echo isset($entity->id) ? $entity->id : ''; ?>">
+		</div>
+
+		<div class="form-group">
+			<label for="product_model">Product Model</label>
+			<input type="text" class="form-control" id="product_model" name="product_model" value="<?php echo isset($entity->product_model) ? $entity->product_model : ''; ?>" required>
+		</div>
+
+		<div class="form-group">
+			<label for="product_name">Product Year</label>
+			<input type="text" class="form-control" id="product_year" name="product_year" value="<?php echo isset($entity->product_year) ? $entity->product_year : ''; ?>" required>
+		</div>
+
+		<div class="form-group">
+			<label for="description">Description</label>
+			<input type="text" class="form-control" id="description" name="description" value="<?php echo isset($entity->description) ? $entity->description : ''; ?>">
+		</div>
+
+		<div class="form-group">
+			<label for="lot_size">Lot Size</label>
+			<input type="number" class="form-control" id="lot_size" name="lot_size" value="<?php echo isset($entity->lot_size) ? $entity->lot_size : ''; ?>" required>
+		</div>
+
+		<div class="form-group">
+			<label for="cp_id">CP</label>
+			<select name="cp_id" id="cp_id" class="form-control selectpicker" data-live-search="true">
+				<option></option>
+
+				<?php foreach($cp_items as $row): ?>
+					<option value="<?php echo $row->id; ?>" <?php echo isset($entity->cp_id) ? $entity->cp_id == $row->id ? 'selected' : '' : ''; ?> >
+						<?php echo $row->model . ' - '. $row->year_model; ?>
+					</option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+
+	</div>
+	
+	<div class="modal-footer">
+		<div class="form-group">
+			<button type="button" class="btn btn-flat btn-info pull-left" data-dismiss="modal">Close</button>
+			<input type="submit" value="Submit" class="btn btn-flat btn-danger">
+		</div>
+	</div>
+	
+</form><!-- End Form -->
+<script src="<?php echo base_url('resources/plugins/select/js/bootstrap-select.min.js');?>"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#cp_id').selectpicker({});
+	});
+</script>
+					
---
+++ application/views/portcode/list_view.php
@@ -0,0 +1,83 @@
+<!-- Items block -->
+<section class="content vin_control">
+	<!-- row -->
+	<div class="row">
+		<!-- col-md-6 -->
+		<div class="col-md-6">
+			<!-- Box danger -->
+			<?php echo $this->session->flashdata('message'); ?>
+
+			<div class="box box-danger">
+				<!-- Content -->
+				<div class="box-header with-border">
+					<a href="<?php echo base_url('index.php/vin/form') ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+						<button class="btn btn-flat btn-success pull-right">Add Vin Model <i class="fa fw fa-plus" aria-hidden="true"></i></button>
+					</a>
+				</div>
+
+				<div class="box-body">
+					<!-- Item table -->
+					<table class="table table-condensed table-striped table-bordered">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Product Model</th>
+								<th>Product Year</th>
+								<th>Description</th>
+								<th>Lot Size</th>
+								<th></th>
+								<th></th>
+							</tr>
+						</thead>
+
+						<tbody>
+							<?php $count = 1; ?>
+							<?php foreach ($entities as $entity): ?>
+								<tr>
+									<td><?php echo $count; ?></td>
+									<td><?php echo $entity->product_model; ?></td>
+									<td><?php echo $entity->product_year; ?></td>
+									<td><?php echo $entity->description; ?></td>
+									<td><?php echo $entity->lot_size; ?></td>
+									<td>
+										<a href="<?php echo base_url('index.php/vin/form/' . $entity->id); ?>"  data-toggle="modal" data-target=".bs-example-modal-sm">
+											<i class="fa fa-pencil" aria-hidden="true"></i>
+										</a>
+									</td>
+									<td>
+										<a href="<?php echo base_url('index.php/vin/notice/' . $entity->id); ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+											<i class="fa fa-trash" aria-hidden="true"></i>
+										</a>
+									</td>
+								</tr>
+								<?php $count++; ?>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+					<!-- End of table -->
+				</div>
+				<!-- End of content -->
+			</div>
+			<!-- End of danger -->
+		</div>
+		<!-- End of col-md-6 -->
+	</div>
+	<!-- End of row -->
+</section>
+<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+  <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content">
+      ...
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('.table').DataTable();
+	});
+
+	// Detroy modal
+	$('body').on('hidden.bs.modal', '.modal', function () {
+		$(this).removeData('bs.modal');
+	}); 
+</script>
