76fc1873a37bd00c59b1325e251006f7f1c377ae
Jerome <jerome-fabricante@isuzuphil.com>
2017-10-04 16:47:05 +0800

    Create model for color

---
+++ application/models/Color_model.php
@@ -0,0 +1,68 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Color_model extends CI_Model {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->load->database();
+	}
+
+	public function browse(array $params = array('type' => 'object'))
+	{
+		if ($params['type'] == 'object')
+		{
+			return $this->db->get('color_tbl')->result();	
+		}
+
+		return $this->db->get('color_tbl')->result_array();
+	}
+
+	public function read(array $params = array('type' => 'object'))
+	{
+		if ($params['id'] > 0)
+		{
+			$query = $this->db->get_where('color_tbl', array('id' => $params['id']));
+
+			if ($params['type'] == 'object')
+			{
+				return $query->row();
+			}
+			
+			return $query->row_array();
+		}
+	}
+
+	public function store($params)
+	{
+		$id = $this->input->post('id') ? $this->input->post('id') : 0;
+
+		if ($id > 0)
+		{
+			$this->db->update('color_tbl', $params, array('id' => $id));
+		}
+		else
+		{
+			$this->db->insert('color_tbl', $params);
+		}
+
+		return $this;	
+	}
+
+	public function delete()
+	{
+		$id = $this->input->post('id');
+
+		$this->db->delete('color_tbl', array('id' => $id));
+
+		return $this;
+	}
+
+	public function exist($params)
+	{
+		$query = $this->db->get_where('color_tbl', array('product_model' => $params['product_model']));
+
+		return $query->num_rows();
+	}
+}
