c19914b6e2fd6e31005d17b67581eba624fbdb0a
Jerome <jerome-fabricante@isuzuphil.com>
2017-10-04 17:04:38 +0800

    Color list view

---
+++ application/views/color/list_view.php
@@ -0,0 +1,79 @@
+<!-- Items block -->
+<section class="content color">
+	<!-- row -->
+	<div class="row">
+		<!-- col-md-6 -->
+		<div class="col-md-5">
+			<!-- Box danger -->
+			<?php echo $this->session->flashdata('message'); ?>
+
+			<div class="box box-danger">
+				<!-- Content -->
+				<div class="box-header with-border">
+					<a href="<?php echo base_url('index.php/color/form') ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+						<button class="btn btn-flat btn-success pull-right">Add Color <i class="fa fw fa-plus" aria-hidden="true"></i></button>
+					</a>
+				</div>
+
+				<div class="box-body">
+					<!-- Item table -->
+					<table class="table table-condensed table-striped table-bordered">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Color Code</th>
+								<th>Color Name</th>
+								<th></th>
+								<th></th>
+							</tr>
+						</thead>
+
+						<tbody>
+							<?php $count = 1; ?>
+							<?php foreach ($entities as $entity): ?>
+								<tr>
+									<td><?php echo $count; ?></td>
+									<td><?php echo $entity->code; ?></td>
+									<td><?php echo $entity->name; ?></td>
+									<td>
+										<a href="<?php echo base_url('index.php/color/form/' . $entity->id); ?>"  data-toggle="modal" data-target=".bs-example-modal-sm">
+											<i class="fa fa-pencil" aria-hidden="true"></i>
+										</a>
+									</td>
+									<td>
+										<a href="<?php echo base_url('index.php/color/notice/' . $entity->id); ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+											<i class="fa fa-trash" aria-hidden="true"></i>
+										</a>
+									</td>
+								</tr>
+								<?php $count++; ?>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+					<!-- End of table -->
+				</div>
+				<!-- End of content -->
+			</div>
+			<!-- End of danger -->
+		</div>
+		<!-- End of col-md-6 -->
+	</div>
+	<!-- End of row -->
+</section>
+<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+  <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content">
+      ...
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('.table').DataTable();
+	});
+
+	// Detroy modal
+	$('body').on('hidden.bs.modal', '.modal', function () {
+		$(this).removeData('bs.modal');
+	}); 
+</script>
