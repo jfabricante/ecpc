11a916d5bec9713779ee1009cda0a0890b2af35c
Unknown <jerome-fabricante@isuzuphil.com>
2017-10-25 16:35:48 +0800

    Process security code
    Update item details

--- application/views/vin_engine/model_view.php
+++ application/views/vin_engine/model_view.php
@@ -14,7 +14,7 @@
 					<form v-on:submit.prevent="fetchModelItems" method="post">
 						<div class="row">
 
-							<div class="col-md-4">
+							<div class="col-md-3">
 								<div class="form-group">
 									<label for="vin_model" class="col-md-4 col-form-label">Vin Model: </label>
 									<div class="col-md-8">
@@ -31,7 +31,7 @@
 							<div class="col-md-3">
 								<div class="form-group">
 									<label for="lot_from" class="col-md-4 col-form-label">Lot from: </label>
-									<div class="col-md-7">
+									<div class="col-md-8">
 										<select name="lot_from" id="lot_from" ref="lot_from" class="select2 form-control">
 											<option></option>
 											<option v-for="entity of lot" v-bind:value="entity.LOT_NO">
@@ -42,10 +42,10 @@
 								</div>
 							</div>
 
-							<div class="col-md-3">
+							<div class="col-md-2">
 								<div class="form-group">
-									<label for="lot_to" class="col-md-3 col-form-label">to: </label>
-									<div class="col-md-7">
+									<label for="lot_to" class="col-md-2 col-form-label">to: </label>
+									<div class="col-md-10">
 										<select name="lot_to" id="lot_to" ref="lot_to" class="select2 form-control">
 											<option></option>
 											<option v-for="entity of lot" v-bind:value="entity.LOT_NO">
@@ -57,10 +57,23 @@
 							</div>
 							
 							<!-- submit -->
-							<div class="col-md-2">
+							<div class="col-md-1">
 								<button type="submit" class="btn btn-flat btn-danger">Filter Data</button>
 							</div>
 							<!-- ./submit -->
+
+							<!-- Set security number -->
+							<div class="col-md-1">
+								<button type="button" class="btn btn-flat btn-info" v-on:click="fetchLastNumber">Set Security</button>
+							</div>
+							<!-- ./Set security number -->
+
+							<!-- Set security number -->
+							<div class="col-md-1">
+								<button type="button" class="btn btn-flat btn-success" v-on:click="updateDetails">Update Details</button>
+							</div>
+							<!-- ./Set security number -->
+
 						</div>
 					</form>
 				</div>
@@ -70,6 +83,8 @@
 					<table class="table table-condensed table-striped table-bordered" id="table">
 						<thead>
 							<tr>
+								<th>#</th>
+								<th>Sequence</th>
 								<th>Model Name</th>
 								<th>Vin No.</th>
 								<th>Engine No.</th>
@@ -93,12 +108,14 @@
 	</div>
 	<!-- End of row -->
 </section>
-<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-  <div class="modal-dialog modal-sm" role="document">
-    <div class="modal-content">
-	...
-    </div>
-  </div>
+<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" id="myModal">
+	<div class="modal-dialog modal-sm" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				
+			</div>
+		</div>
+	</div>
 </div>
 <script src="<?php echo base_url('resources/plugins/select2/js/select2.min.js');?>"></script>
 <script src="<?php echo base_url('resources/js/axios/axios.min.js') ?>"></script>
@@ -107,6 +124,7 @@
 <script type="text/javascript">
 	// Vue Script
 	const appUrl = '<?php echo base_url('index.php') ?>';
+	const tmUrl  = '<?php echo base_url('resources/images/') ?>';
 
 	const app = new Vue({
 		el: '#app',
@@ -116,7 +134,8 @@
 			vinSelected: '',
 			lot: [],
 			lot_from: '',
-			lot_to: ''
+			lot_to: '',
+			security: {},
 		},
 		created() {
 			this.fetchVinModel()
@@ -127,6 +146,9 @@
 			},
 			vinSelected: function() {
 				this.fetchVinLot()
+			},
+			security: function() {
+				this.updateItems()
 			}
 		},
 		mounted() {
@@ -138,6 +160,8 @@
 
 			$(this.$refs.vin_model).on('change', function() {
 				self.vinSelected = $(this).val()
+				console.log(self.vinSelected)
+				self.security = {}
 			});
 
 			$(this.$refs.lot_from).on('change', function() {
@@ -193,6 +217,16 @@
 					console.log(err.message);
 				});
 			},
+			fetchLastNumber: function() {
+				axios.get(appUrl + '/security/get_last_number')
+				.then((response) => {
+					this.security = response.data
+					console.log(this.security)
+				})
+				.catch((err) => {
+					console.log(err.message);
+				});
+			},
 			fetchModelItems: function() {
 				axios({
 					url: appUrl + '/vin_engine/ajax_fetch_model_items',
@@ -218,6 +252,54 @@
 					console.log(error)
 				});
 			},
+			updateDetails: function() {
+				this.showModal()
+				axios({
+					url: appUrl + '/vin_engine/ajax_update_details',
+					method: 'post',
+					contentType: "application/xml; charset=utf-8",
+					data: {
+						items: this.vinModelItems || '',
+						security: this.security || '',
+					}
+				})
+				.then((response) => {
+					$('#myModal').modal('hide')
+
+					window.open(appUrl + '/vin_engine/download_pdf')
+				})
+				.catch((error) => {
+					// your action on error success
+					console.log(error)
+				});
+			},
+			updateItems: function()
+			{
+				if (this.vinModelItems.length > 0)
+				{
+					for (entity of this.vinModelItems)
+					{
+						this.securitySequence()
+						entity.SECURITY_NO = this.security.SECURITY_NO
+					}
+
+					// Re-render the table data
+					this.drawTableContent()
+				}
+			},
+			securitySequence: function()
+			{
+				// Array of restriction
+				const excep = ['0000', '1111', '2222', '3333', '4444', '5555', '6666', '7777', '8888', '9999']
+
+				this.security.SECURITY_NO = Number(this.security.SECURITY_NO) + 1
+
+				// Increment by 1 if the value is in excep array
+				if (_.includes(excep, this.security.SECURITY_NO.toString()))
+				{
+					this.security.SECURITY_NO = Number(this.security.SECURITY_NO) + 1
+				}
+			},
 			// Add rows using datatables method
 			drawTableContent: function() {
 				let $table = $('#table').DataTable();
@@ -226,12 +308,21 @@
 				$table.clear().draw();
 
 				// Insert the new content
+				let i = 1;
 				for (let entity of this.vinModelItems)
 				{
-					$table.row.add([ entity.PRODUCT_MODEL, entity.VIN_NO, entity.ENGINE_NO, entity.SECURITY_NO, entity.LOT_NO,
+					$table.row.add([i, entity.SEQUENCE, entity.PRODUCT_MODEL, entity.VIN_NO, entity.ENGINE_NO, entity.SECURITY_NO, entity.LOT_NO,
 					entity.COLOR, entity.INVOICE_NO ]).draw().node();
+					i++
 				}
-			}
+			},
+			showModal: function() {
+				$("#myModal").modal({backdrop: 'static', keyboard: false})
+
+				$('#myModal').on('shown.bs.modal', function() {
+					$(this).find('.modal-body').html('<img src="' + tmUrl + 'loading.gif" class="img-responsive"/>')
+				});
+			},
 		}
 	})
 
