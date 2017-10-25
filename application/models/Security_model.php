2f7a73d9027e523345667476af266fb922f3bb89
Unknown <jerome-fabricante@isuzuphil.com>
2017-10-25 16:28:20 +0800

    Create security model

---
+++ application/models/Security_model.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Security_model extends CI_Model {
+
+	private $oracle;
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->oracle = $this->load->database('oracle', true);
+	}
+
+	public function browse(array $params = array('type' => 'object'))
+	{
+		if ($params['type'] == 'object')
+		{
+			return $this->oracle->get('SECURITY')->result();	
+		}
+
+		return $this->oracle->get('SECURITY')->result_array();
+	}
+
+	public function read(array $params = array('type' => 'object'))
+	{
+		if ($params['id'] > 0)
+		{
+			$query = $this->oracle->get_where('SECURITY', array('ID' => $params['id']));
+
+			if ($params['type'] == 'object')
+			{
+				return $query->row();
+			}
+			
+			return $query->row_array();
+		}
+	}
+
+	public function store($params)
+	{
+		$id = $this->input->post('ID') ? $this->input->post('ID') : 0;
+
+		if ($id > 0)
+		{
+			$this->oracle->update('SECURITY', $params, array('ID' => $id));
+		}
+		else
+		{
+			$this->oracle->insert('SECURITY', $params);
+		}
+
+		return $this;	
+	}
+
+	public function delete()
+	{
+		$id = $this->input->post('ID');
+
+		$this->oracle->delete('SECURITY', array('ID' => $id));
+
+		return $this;
+	}
+
+	public function exist($params)
+	{
+		$query = $this->oracle->get_where('SECURITY', array('product_model' => $params['product_model']));
+
+		return $query->num_rows();
+	}
+
+	public function fetchLastSecurity()
+	{
+		$query = $this->oracle->order_by('ID', 'DESC')
+				->get('SECURITY');
+
+		return $query->row();
+	}
+
+	/*public function migrateItems()
+	{
+		$old_data = $this->browse(array('type' => 'array'));
+
+		$this->oracle->insert_batch('SECURITY', $old_data);
+
+		return $old_data;
+	}*/
+}
