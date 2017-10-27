b96af5237b833139bb0531ca77beac24f2a2c8c7
Unknown <jerome-fabricante@isuzuphil.com>
2017-10-27 17:33:17 +0800

    Return to IPC Central

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -2,7 +2,7 @@
 <header class="main-header">
 
 	<!-- Logo -->
-	<a href="#" class="logo">
+	<a href="http://172.16.1.34/ipc_central/main_home.php" class="logo">
 		<!-- mini logo for sidebar mini 50x50 pixels -->
 		<span class="logo-mini"><b>IPC</b></span>
 		<!-- logo for regular state and mobile devices -->
