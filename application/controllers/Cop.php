384bf427f6d20d73e20ba92d63b23e0e313e74fb
Unknown <jerome-fabricante@isuzuphil.com>
2017-11-07 12:57:38 +0800

    Generate excel file

--- application/controllers/Cop.php
+++ application/controllers/Cop.php
@@ -124,4 +124,101 @@ class Cop extends CI_Controller {
 		}
 	}	
 
+	protected function _excelReport($params)
+	{
+		if (count($params))
+		{
+			// Create an instance of PHPExcel
+			$excelObj          = new PHPExcel();
+			$excelActiveSheet  = $excelObj->getActiveSheet();
+			$excelDefaultStyle = $excelObj->getDefaultStyle();
+
+			$excelDefaultStyle->getFont()->setSize(10)->setName('Times New Roman');
+
+			// Set the Active sheet
+			$excelObj->setActiveSheetIndex(0);
+
+			// Set alignment
+			$excelDefaultStyle->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+
+			// Add header to the excel
+			$excelActiveSheet->setCellValue('A1', 'CP No.')
+							->setCellValue('B1', 'CP Date')
+							->setCellValue('C1', 'Invoice No.')
+							->setCellValue('D1', 'Entry No.')
+							->setCellValue('E1', 'Model')
+							->setCellValue('F1', 'Lot No.')
+							->setCellValue('G1', 'Quantity')
+							->setCellValue('H1', 'ETD')
+							->setCellValue('I1', 'ETA')
+							->setCellValue('J1', 'Payment Date')
+							->setCellValue('K1', 'Trasmittal Date');
+
+			$excelActiveSheet->getStyle('A1:K1')->getAlignment()->setWrapText(true); 
+
+			$excelActiveSheet->fromArray($params, NULL, 'A2');
+
+			// Paper Orientation
+			$excelActiveSheet->getPageSetup()
+				    ->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
+
+			// Paper Size
+			$excelActiveSheet->getPageSetup()
+				->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
+
+			// Set margins
+			$excelActiveSheet->getPageMargins()->setTop(0.25);
+			$excelActiveSheet->getPageMargins()->setRight(0.25);
+			$excelActiveSheet->getPageMargins()->setLeft(0.25);
+			$excelActiveSheet->getPageMargins()->setBottom(0.25);
+
+			// Apply the column title on every pages
+			$excelActiveSheet->getPageSetup()->setRowsToRepeatAtTopByStartAndEnd(1,1);
+
+			// Fit the content to page
+			$excelActiveSheet->getPageSetup()->setFitToWidth(1);    
+			$excelActiveSheet->getPageSetup()->setFitToHeight(0);
+
+
+			// Apply background color on cell
+			$excelActiveSheet->getStyle('A1:K1')
+							->getFill()
+							->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
+							->getStartColor()
+							->setRGB('ffff99');
+
+			// Set the alignment for the whole document
+			$excelDefaultStyle->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+
+			$style = array(
+				        'borders' => array(
+				            'allborders' => array(
+				                'style' => PHPExcel_Style_Border::BORDER_THIN,
+				                'color' => array('rgb' => '000000')
+				            )
+				        ),
+				        'alignment' => array(
+				        	'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+							'vertical'   => PHPExcel_Style_Alignment::VERTICAL_CENTER
+				        ),
+				    );
+
+			// Apply header style
+			$excelActiveSheet->getStyle("A1:K1")->applyFromArray($style);
+
+			// Excel filename
+			$filename = 'cp.xlsx';
+
+			// Content header information
+			header('Content-Type: application/vnd.ms-excel'); //mine type
+			header('Content-Disposition: attachment; filename="' . $filename . '"');
+			header('Cached-Control: max-age=0');
+
+			// Generate excel version using Excel 2017
+			$objWriter = PHPExcel_IOFactory::createWriter($excelObj, 'Excel2007');
+
+			$objWriter->save('php://output');
+		}
+	
+	}
 }
