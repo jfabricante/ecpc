7f0d15a632567e18083950d1c13f68ab35e0a40c
Unknown <jerome-fabricante@isuzuphil.com>
2018-01-05 08:16:52 +0800

    Limit the visibility of the other users

--- application/views/cop/list_view.php
+++ application/views/cop/list_view.php
@@ -11,40 +11,40 @@
 
 			<div class="box box-danger">
 				<!-- Content -->
-				<div class="box-header with-border">
-					<form class="form-inline" action="<?php echo base_url('index.php/cop/handle_excel_report'); ?>" method="post">
-						<div class="form-group">
-							<label for="from">Date Covered From:</label>
-							<input type="text" class="form-control datepicker" name="from" id="from" required>
-						</div>
+				<?php if (in_array($this->session->userdata('user_access'), array('Administrator', 'ReadOnly'))): ?>
+					<div class="box-header with-border">
+						<form class="form-inline" action="<?php echo base_url('index.php/cop/handle_excel_report'); ?>" method="post">
+							<div class="form-group">
+								<label for="from">Date Covered From:</label>
+								<input type="text" class="form-control datepicker" name="from" id="from" required>
+							</div>
 
-						<div class="form-group">
-							<label for="to">To:</label>
-							<input type="text" class="form-control datepicker" name="to" id="to" required />
-						</div>
+							<div class="form-group">
+								<label for="to">To:</label>
+								<input type="text" class="form-control datepicker" name="to" id="to" required />
+							</div>
 
-						<div class="form-group">
-							<select name="field" id="field" class="form-control selectpicker" data-live-search="true" required>
-								<option></option>
-								<option value="CP_DATE">CP DATE</option>
-								<option value="ETD">ETD</option>
-								<option value="ETA">ETA</option>
-								<option value="PAYMENT_DATE">PAYMENT DATE</option>
-								<option value="TRANSMITTAL_DATE">TRANSMITTAL DATE</option>
-							</select>
-						</div>
+							<div class="form-group">
+								<select name="field" id="field" class="form-control selectpicker" data-live-search="true" required>
+									<option></option>
+									<option value="CP_DATE">CP DATE</option>
+									<option value="ETD">ETD</option>
+									<option value="ETA">ETA</option>
+									<option value="PAYMENT_DATE">PAYMENT DATE</option>
+									<option value="TRANSMITTAL_DATE">TRANSMITTAL DATE</option>
+								</select>
+							</div>
 
-						<div class="form-group">
-							<input type="submit" class="btn btn-flat btn-danger" value="Create Report" />
-						</div>
+							<div class="form-group">
+								<input type="submit" class="btn btn-flat btn-danger" value="Create Report" />
+							</div>
 
-						<a href="<?php echo base_url('index.php/cop/form') ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
-							<button class="btn btn-flat btn-success pull-right">Add CP <i class="fa fw fa-plus" aria-hidden="true"></i></button>
-						</a>
-					</form>
-
-				</div>
-
+							<a href="<?php echo base_url('index.php/cop/form') ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+								<button class="btn btn-flat btn-success pull-right">Add CP <i class="fa fw fa-plus" aria-hidden="true"></i></button>
+							</a>
+						</form>
+					</div>
+				<?php endif ?>
 				<div class="box-body">
 					<!-- Item table -->
 					<table class="table table-condensed table-striped table-bordered">
@@ -62,8 +62,10 @@
 								<th>ETA</th>
 								<th>Payment Date</th>
 								<th>Transmittal Date</th>
-								<th></th>
-								<th></th>
+								<?php if (in_array($this->session->userdata('user_access'), array('Administrator', 'ReadOnly'))): ?>
+									<th></th>
+									<th></th>
+								<?php endif ?>
 							</tr>
 						</thead>
 
@@ -83,21 +85,22 @@
 									<td><?php echo $entity->ETA ? date('m/d/Y', strtotime($entity->ETA)) : ''; ?></td>
 									<td><?php echo $entity->PAYMENT_DATE ? date('m/d/Y', strtotime($entity->PAYMENT_DATE)) : ''; ?></td>
 									<td><?php echo $entity->TRANSMITTAL_DATE ? date('m/d/Y', strtotime($entity->TRANSMITTAL_DATE)) : ''; ?></td>
-
-									<td>
-										<?php if ($entity->ID): ?>
-											<a href="<?php echo base_url('index.php/cop/form/' . $entity->ID); ?>"  data-toggle="modal" data-target=".bs-example-modal-sm">
-												<i class="fa fa-pencil" aria-hidden="true"></i>
-											</a>
-										<?php endif ?>
-									</td>
-									<td>
-										<?php if ($entity->ID): ?>
-											<a href="<?php echo base_url('index.php/cop/notice/' . $entity->ID); ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
-												<i class="fa fa-trash" aria-hidden="true"></i>
-											</a>
-										<?php endif ?>
-									</td>
+									<?php if (in_array($this->session->userdata('user_access'), array('Administrator', 'ReadOnly'))): ?>
+										<td>
+											<?php if ($entity->ID): ?>
+												<a href="<?php echo base_url('index.php/cop/form/' . $entity->ID); ?>"  data-toggle="modal" data-target=".bs-example-modal-sm">
+													<i class="fa fa-pencil" aria-hidden="true"></i>
+												</a>
+											<?php endif ?>
+										</td>
+										<td>
+											<?php if ($entity->ID): ?>
+												<a href="<?php echo base_url('index.php/cop/notice/' . $entity->ID); ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+													<i class="fa fa-trash" aria-hidden="true"></i>
+												</a>
+											<?php endif ?>
+										</td>
+									<?php endif ?>
 								</tr>
 								<?php $count++; ?>
 							<?php endforeach; ?>
